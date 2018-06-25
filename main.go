cab653309d9fbb7d39ec8cd6b92b935bc549cacf
Devin Checa <devincheca@gmail.com>
2018-06-24 22:46:47 -0700

    create/read functionality is complete, update and delete needs to be added

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ type input struct {
   Data string
 }
 
+type query struct {
+  Index string
+}
+
 func create(w http.ResponseWriter, r *http.Request) {
   body, err := ioutil.ReadAll(r.Body)
   if err != nil {
@@ -31,7 +35,6 @@ func create(w http.ResponseWriter, r *http.Request) {
     panic("An index is required to create a doc.")
   }
   filename := fmt.Sprintf("data/data" + fromReq.Index + ".json")
-  fmt.Println(filename)
   file, err := os.Create(filename)
   if err != nil {
     panic(err)
@@ -39,12 +42,32 @@ func create(w http.ResponseWriter, r *http.Request) {
   defer file.Close()
   contents := fmt.Sprintf("{\"Index:\":\"" + fromReq.Index + "\",\"Data\":\"" + fromReq.Data + "\"}")
   file.WriteString(contents)
-  fmt.Println("File write complete")
-  fmt.Fprint(w, "File write complete")
+  fmt.Println(filename + " write complete")
+  fmt.Fprint(w, filename + " write complete")
+}
+
+func read(w http.ResponseWriter, r *http.Request) {
+  body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    panic(err)
+  }
+  var fromReq query
+  err = json.Unmarshal(body, &fromReq)
+  if err != nil {
+    panic(err)
+  }
+  filename := fmt.Sprintf("data/data" + fromReq.Index + ".json")
+  stream, err := ioutil.ReadFile(filename)
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println(string(stream))
+  fmt.Fprint(w, string(stream))
 }
 
 func main() {
   http.HandleFunc("/create", create)
+  http.HandleFunc("/read", read)
   fmt.Println("listening on port 3000...")
   if err := http.ListenAndServe(":3000", nil); err != nil {
     log.Fatal(err)
