c3dc75b3543bdc50023d3978f9e7c1ea199b6fed
Devin Checa <devincheca@gmail.com>
2018-06-24 21:20:26 -0700

    need to write and create files

--- main.go
+++ main.go
@@ -2,9 +2,40 @@ package main
 
 import
 (
+  "encoding/json"
   "fmt"
+  "io/ioutil"
+  "log"
+  "net/http"
 )
 
+type input struct {
+  index int
+  data string
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+  body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    panic(err)
+  }
+  var fromReq input
+  err = json.Unmarshal(body, &fromReq)
+  if err != nil {
+    panic(err)
+  }
+  if fromReq.index == 0 {
+    fmt.Print(w, "Request received without index.")
+    fmt.Fprint(w, "An index is required to create a doc.")
+    panic("An index is required to create a doc.")
+  }
+  fmt.Println("continue with file write")
+}
+
 func main() {
-  fmt.Println("test")
+  http.HandleFunc("/create", create)
+  fmt.Println("listening on port 3000...")
+  if err := http.ListenAndServe(":3000", nil); err != nil {
+    log.Fatal(err)
+  }
 }
